5d2d6e0e34f95f5d0e85fdf96aa084a636e43fa7
Mizuki Zenta <mizukisb@gmail.com>
2018-08-10 12:50:24 +1000

    add humburger menu

--- app/src/main/java/com/example/mizuki/rentmeproject/HomeActivity.java
+++ app/src/main/java/com/example/mizuki/rentmeproject/HomeActivity.java
@@ -1,37 +1,79 @@
 package com.example.mizuki.rentmeproject;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button btnLogout;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // inti views
-        btnLogout = findViewById(R.id.btnLogout);
+        // init view
+        drawerLayout = findViewById(R.id.activity_main);
+        navigationView = findViewById(R.id.nv);
 
-        // set click event
-        btnLogout.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
-                // sign out from firebase auth
-                FirebaseAuth.getInstance().signOut();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-              // redirect to login page
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-                finish();
+
+        // click event for the navigation items
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.itemPost:
+
+                        case R.id.itemAccount:
+
+                    case R.id.itemMessageBox:
+
+                    case R.id.itemPostbox:
+
+                    case R.id.itemHelp:
+
+                            case R.id.itemLogout:
+                                // when logout is clicked sign out form firebase auth
+                                FirebaseAuth.getInstance().signOut();
+                                // redirect to login page
+                                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                                 finish();
+                                default:
+                                    return true;
+                                    }
             }
         });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
 
