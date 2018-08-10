384ed63a957c895be10633120f84bdb4d57b02de
Mizuki Zenta <mizukisb@gmail.com>
2018-08-10 17:22:22 +1000

    add autocomplete api and spiner

--- app/src/main/java/com/example/mizuki/rentmeproject/PostActivity.java
+++ app/src/main/java/com/example/mizuki/rentmeproject/PostActivity.java
@@ -1,13 +1,65 @@
 package com.example.mizuki.rentmeproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 public class PostActivity extends AppCompatActivity {
+
+    private EditText postLocation;
+    private int AUTO_COMP_REQ_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        // init views
+        postLocation = findViewById(R.id.postLocation);
+
+        // click event
+        postLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autoCompleteForm();
+            }
+        });
+
     }
+
+    // get data form autocomplete form
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == AUTO_COMP_REQ_CODE){
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                CharSequence location = place.getAddress().toString();
+
+                postLocation.setText(location);
+
+            }
+        }
+    }
+
+    //  call google auto complete apt
+    public void autoCompleteForm(){
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this);
+            startActivityForResult(intent, AUTO_COMP_REQ_CODE);
+        } catch (Exception e) {
+            Log.e("Error", e.getStackTrace().toString());
+        }
+    }
+
 }
