2ff6ba8963deab65090793ca9a7a20677ad60a68
Mizuki Zenta <mizukisb@gmail.com>
2018-08-19 20:17:12 +1000

    change the card design

--- app/src/main/java/Helper/ItemListAdapter.java
+++ app/src/main/java/Helper/ItemListAdapter.java
@@ -1,13 +1,17 @@
 package Helper;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.mizuki.rentmeproject.R;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 import java.util.Map;
@@ -23,12 +27,35 @@ public class ItemListAdapter extends SimpleAdapter {
         // here you let SimpleAdapter built the view normally.
         View v = super.getView(position, convertView, parent);
 
-        // Then we get reference for Picasso
+        // get reference
         ImageView img = (ImageView) v.getTag();
         if(img == null){
             img = v.findViewById(R.id.itemImg);
             v.setTag(img);
         }
+
+        TextView rentedText = v.findViewById(R.id.itemRented);
+        if(rentedText == null){
+            rentedText = v.findViewById(R.id.itemRented);
+            v.setTag(rentedText);
+        }
+
+        // get the rented attribute and check the boolean value
+        @SuppressWarnings("unchecked")
+        Boolean isRented = ((Map<String, Boolean>)getItem(position)).get("rented");
+
+        if(isRented) {
+            // if the item is rented
+            rentedText.setText("Rented");
+            // set opacity for the rented item
+            img.setAlpha(0.4f);
+        }else{
+            // if the item is not rented
+            rentedText.setText(null);
+            img.setAlpha(1.0f);
+        }
+
+
 
         // get the url from the data the value of key "image" to the `Map`
         @SuppressWarnings("unchecked")
