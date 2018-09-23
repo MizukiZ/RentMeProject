090b8054be87b00b985f448534fd042a0d4aadc1
Mizuki Zenta <mizukisb@gmail.com>
2018-09-23 14:17:40 +1000

    add delere menu for list view

--- app/src/main/java/com/example/mizuki/rentmeproject/OwnPostItemsActivity.java
+++ app/src/main/java/com/example/mizuki/rentmeproject/OwnPostItemsActivity.java
@@ -1,13 +1,19 @@
 package com.example.mizuki.rentmeproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -34,6 +40,8 @@ public class OwnPostItemsActivity extends AppCompatActivity {
     FirebaseUser currentFirebaseUser;
     String userID;
 
+    PopupMenu popupMenu;
+
 
 
     @Override
@@ -50,7 +58,6 @@ public class OwnPostItemsActivity extends AppCompatActivity {
         userID = currentFirebaseUser.getUid();
 
 
-
         // make instance of custom simple adapter and put data in
         itemListAdapter = new ItemListAdapter(this,
                 itemListData, // data you want to use
@@ -60,9 +67,12 @@ public class OwnPostItemsActivity extends AppCompatActivity {
 
 
         // get post item the user posted
-        db.child("Post").orderByChild("user_id").equalTo(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+        db.child("Post").orderByChild("user_id").equalTo(userID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // init the itemList Data
+                itemListData.clear();
 
                 // loop the data to generate item list array
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
@@ -100,14 +110,67 @@ public class OwnPostItemsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                //get clicked view object
+//                get clicked view object
                 @SuppressWarnings("unchecked")
                 HashMap<String, Object> itemObject = (HashMap<String, Object>) itemListAdapter.getItem(position);
 
-                Intent detailPageIntent = new Intent(OwnPostItemsActivity.this, ItemDetailActivity.class);
-                detailPageIntent.putExtra("itemObject", itemObject);
-                OwnPostItemsActivity.this.startActivity(detailPageIntent);
+
+                PopupMenu popup = new PopupMenu(OwnPostItemsActivity.this, view);
+                popup.getMenuInflater().inflate(R.menu.own_post_list_menu, popup.getMenu());
+                //
+                popup.show();
+
+                // set popup menu event listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        String operation = item.getTitle().toString();
+                        switch (operation){
+                            case("EDIT"):
+
+                                Intent detailPageIntent = new Intent(OwnPostItemsActivity.this, ItemDetailActivity.class);
+                                detailPageIntent.putExtra("itemObject", itemObject);
+                                OwnPostItemsActivity.this.startActivity(detailPageIntent);
+
+                                return true;
+                            case("DELETE"):
+
+                                // show confirm dialog
+                                AlertDialog.Builder deleteDialog = new AlertDialog.Builder(OwnPostItemsActivity.this);
+                                deleteDialog.setMessage("Are you sure?");
+                                deleteDialog.setCancelable(true);
+
+                                deleteDialog.setPositiveButton(
+                                        "Yes",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // delete corresponding data
+
+                                                db.child("Post").child(itemObject.get("id").toString()).removeValue();
+
+                                            }
+                                        });
+
+                                deleteDialog.setNegativeButton(
+                                        "No",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+//                                           //dismiss the dialog
+                                                dialog.cancel();
+                                            }
+                                        });
+
+                                AlertDialog alert11 = deleteDialog.create();
+                                alert11.show();
+
+                                return true;
+                        }
+                        return true;
+                    }
+                });
             }
         });
+
+
     }
 }
